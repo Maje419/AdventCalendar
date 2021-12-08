f9809b89eb9752e733b390f243c0b95543b3127f
Mathias Jensen <maje419@student.sdu.dk>
2021-12-08 16:38:44 +0100

    Cleaning

--- 4/Main.java
+++ 4/Main.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Currency;
-
-import javax.swing.plaf.basic.BasicBorders;
 
 public class Main{
 
